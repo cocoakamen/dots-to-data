71e2445628ccf4ebe44d70291d0d73ba734acae8
cocoakamen <44543580+cocoakamen@users.noreply.github.com>
2024-03-16 12:27:41 +0000

    Reactのimport文を修正しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import HistogramDots from './components/HistogramDots';
 import HistogramConfig from './components/HistogramConfig';
--- src/components/HistogramConfig.tsx
+++ src/components/HistogramConfig.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 const HistogramConfig: React.FC = () => {
     return (
